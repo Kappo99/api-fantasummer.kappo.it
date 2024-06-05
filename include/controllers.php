dd641c8cc9ba4652780c2754514b9d4350906e9d
Kappo <andrea30.4.99@gmail.com>
2024-06-05 13:56:02 +0200

    Rimossa Formazione

--- include/controllers.php
+++ include/controllers.php
@@ -3,4 +3,3 @@
 require_once SERVER_ROOT . 'controllers/auth.php';
 require_once SERVER_ROOT . 'controllers/giocatori.php';
 require_once SERVER_ROOT . 'controllers/eventi.php';
-require_once SERVER_ROOT . 'controllers/formazioni.php';
--- include/model.php
+++ include/model.php
@@ -3,4 +3,3 @@
 require_once SERVER_ROOT . 'model/Log.php';
 require_once SERVER_ROOT . 'model/Giocatore.php';
 require_once SERVER_ROOT . 'model/Evento.php';
-require_once SERVER_ROOT . 'model/Formazione.php';
